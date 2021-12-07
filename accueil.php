60877c8f3584544e69994672a71bb0d23eb7cd7a
Vincent <vincent.pompei@edu.ece.fr>
2021-12-07 13:36:26 +0100

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -53,12 +53,65 @@
         </div>
     </nav>
 
+    <div class="container">
+      <h2>Carousel Example</h2>
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner">
+
+          <div class="item active">
+            <img src="IMG/T-Shirt1.jpg" style="width:100%;">
+            <div class="carousel-caption">
+              <h3>T-Shirt</h3>
+          </div>
+      </div>
+
+      <div class="item">
+        <img src="IMG/pantalon1.jpg" style="width:100%;">
+        <div class="carousel-caption">
+          <h3>Pantalon</h3>
+      </div>
+  </div>
+
+  <div class="item">
+    <img src="IMG/sweat1.jpg"style="width:100%;">
+    <div class="carousel-caption">
+      <h3>Sweat</h3>
+  </div>
+</div>
+
+</div>
+
+<!-- Left and right controls -->
+<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+  <span class="glyphicon glyphicon-chevron-left"></span>
+  <span class="sr-only">Previous</span>
+</a>
+<a class="right carousel-control" href="#myCarousel" data-slide="next">
+  <span class="glyphicon glyphicon-chevron-right"></span>
+  <span class="sr-only">Next</span>
+</a>
+</div>
+</div>
 
 
 
-    <footer class="container-fluid text-center">Copyright &copy; 2021 Outfit Crew<br>
-        <a href="mailto:vincent.pompei@edu.ece.fr">vincent.pompei@edu.ece.fr</a>
-    </footer>
+
+
+
+
+
+<footer class="container-fluid text-center">Copyright &copy; 2021 Outfit Crew<br>
+    <a href="mailto:vincent.pompei@edu.ece.fr">vincent.pompei@edu.ece.fr</a>
+</footer>
 
 
 
