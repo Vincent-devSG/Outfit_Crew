dba3498ece41ffe5e80223cc7ab58abd4058737c
Pierrot888 <pierremestre888@gmail.com>
2021-12-08 15:05:39 +0100

    triage tout parcourir

--- ToutParcourir.php
+++ ToutParcourir.php
@@ -7,6 +7,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
     <link href="ToutParcourir.css" rel="stylesheet" type="text/css"/>
+    
 
 </head>
 
@@ -60,114 +61,155 @@
         <h2><div class="well">Découvrez notre gamme de produits</div></h2>
     </div>
 </div>
+<div class="container">
+<div class="well well-sm">Trier par Catégorie</div>
+</div> 
 
- <!-- boutons coulissants pour trier -->
+<form action="ToutParcourir.php" method="post" class="form-inline">
+    <table align="center">
+        <tr>
+            <!--<td colspan="2" align="center"><input type="submit" name="choix" value="choix"></td>-->
+            <td colspan="2" align="center"><input type="submit" name="ToutAfficher" value="Tout Afficher"></td>
+            <td colspan="2" align="center"><input type="submit" name="Rare" value="Articles Rares"></td>
+            <td colspan="2" align="center"><input type="submit" name="HautDeGamme" value="Articles Haut de Gamme"></td>
+            <td colspan="2" align="center"><input type="submit" name="Regulier" value="Articles Reguliers"></td>
+        </tr>
+        <tr><td><br></td></tr>
+    </table>
+</form> 
+<form action="ToutParcourir.php" method="post" class="form-inline">
+    <table align="center">    
+        <div class="container">
+        <div class="well well-sm">Trier par Prix</div>
+        </div>
+        <tr>
+            <td colspan="2" align="center"><input type="submit" name="Normal" value="Normal"></td>
+            <td colspan="2" align="center"><input type="submit" name="Croissant" value="Prix Croissant"></td>
+            <td colspan="2" align="center"><input type="submit" name="Decroissant" value="Prix Decroissant"></td>
+        </tr>
+    </table>
+</form> 
+
+ <!-- boutons coulissants pour trier 
+ <form action="ToutParcourir.php" method="post" class="form-inline">
     <div class="choixTriage">
         <div class="container">  
           <div class="row">
           <div class="col-sm-2">
 
-          <div class="dropdown">
+           <div class="dropdown">
             <button class="btn btn-default dropdown-toggle btn-info" type="button" id="menu1" data-toggle="dropdown">Trier par Type
             <span class="caret"></span></button>
-            <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
-              <li><a href="#">Tout afficher</a></li>
-              <li role="presentation" class="divider"></li>
-              <li><a href="#">Articles Rares</a></li>
-              <li role="presentation" class="divider"></li>
-              <li><a href="#">Articles Haut de Gamme</a></li>
-              <li role="presentation" class="divider"></li>
-              <li><a href="#">Articles Réguliers</a></li>   
+            <table class="dropdown-menu" role="menu" aria-labelledby="menu1">
+              <ul>
+                <li><input type="submit" name="Tout Afficher" value="ToutAfficher"></li>
+                <li><input type="submit" name="Articles Rares" value="Rare"></li>
+                <li><input type="submit" name="Articles Haut de Gamme" value="HautDeGamme"></li>
+                <li><input type="submit" name="Articles Reguliers" value="Regulier"></li>
+            
             </ul>
+          </div> 
+
           </div>
-          </div>
+
           <div class="col-sm-1">
           <div class="dropdown">
             <button class="btn btn-default dropdown-toggle btn-info" type="button" id="menu1" data-toggle="dropdown">Trier par Prix
             <span class="caret"></span></button>
             <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
-              <li><a href="#">Normal</a></li>
+              <li><button class="dropdown-item" align="center" type="button" value="Normal">Normal</button></li>
               <li role="presentation" class="divider"></li>
-              <li><a href="#">Prix Croissant</a></li>
+              <li><button class="dropdown-item" align="center" type="button" value="PrixCroissant">Prix Croissant</button></li>
               <li role="presentation" class="divider"></li>
-              <li><a href="#">Prix Décroissant</a></li>   
+              <li><button class="dropdown-item" align="center" type="button" value="PrixDecroissant">Prix Decroissant</button></li>   
             </ul>
           </div>
           </div>
           </div>
         </div>
     </div>
+    </form>
+-->
 
 <!-- affichage des blocs de produits -->
 <br><br>   
-
-<!--
-<div class="container-fluid text-center bg-grey">
-  
-    <div class="col-sm-4">
-      <div class="thumbnail">
-        <img src="logo.png" alt="Paris" width="400" height="300">
-        <p><strong>Paris</strong></p>
-        <p>Yes, we built Paris</p>
-      </div>
-    </div>
-    <div class="col-sm-4">
-      <div class="thumbnail">
-        <img src="newyork.jpg" alt="New York" width="400" height="300">
-        <p><strong>New York</strong></p>
-        <p>We built New York</p>
-      </div>
-    </div>
-    <div class="col-sm-4">
-      <div class="thumbnail">
-        <img src="sanfran.jpg" alt="San Francisco" width="400" height="300">
-        <p><strong>San Francisco</strong></p>
-        <p>Yes, San Fran is ours</p>
-      </div>
-    </div> -->
-
-    <!--
-        $database = "bdd_shop";
-        $db_handle = mysqli_connect('localhost','root','');
-        $db_found = mysqli_select_db($db_handle,$database);
-
-        $sql = "SELECT * FROM objet";
-
-        $result = mysqli_query($db_handle, $sql);
-
-        echo "<table border=\"1\">";    
-
-        while ($data = mysqli_fetch_assoc($result)) {
- 
-        echo "<tr>";
-        $image = $data['photo'];
-        echo "IMAGE" .$image;
-        echo "<td>" . "<img src='$image' height='400' width='300'>" . "</td>";    
-        echo "<td>" . $data['nom'] . "</td>";
-        echo "<td>" . $data['description'] . "</td>";
-        echo "<td>" . $data['etat'] . "</td>";
-        echo "<td>" . $data['prix'] . "</td>";
-        echo "<td>" . $data['categorie'] . "</td>";
-
-        echo "</tr>";
-        
-    }
-        echo "</table>";
-    -->
-
 
        
 
         <?php
 
+            $TrierType = isset($_POST["choix"])? $_POST["choix"] : "";
+            $TrierType1 = isset($_POST["ToutAfficher"])? $_POST["ToutAfficher"] : "";
+            $TrierType2 = isset($_POST["Rare"])? $_POST["Rare"] : "";
+            $TrierType3 = isset($_POST["HautDeGamme"])? $_POST["HautDeGamme"] : "";
+            $TrierType4 = isset($_POST["Regulier"])? $_POST["Regulier"] : "";
+
+
+            $TrierPrix1 = isset($_POST["Normal"])? $_POST["Normal"] : ""; 
+            $TrierPrix2 = isset($_POST["Croissant"])? $_POST["Croissant"] : ""; 
+            $TrierPrix3 = isset($_POST["Decroissant"])? $_POST["Decroissant"] : ""; 
+
+            /*echo $TrierType;
+            echo $TrierType1;
+            echo $TrierType2;
+            echo $TrierType3;
+            echo $TrierType4;
+
+            echo $TrierPrix1;
+            echo $TrierPrix2;
+            echo $TrierPrix3;*/
+
             $database = "bdd_shop";
+            //echo $database;
 
             $db_handle = mysqli_connect('localhost','root','');
             $db_found = mysqli_select_db($db_handle,$database);
 
-            $sql = "SELECT * FROM objet";
+            $sql = "";
 
-            $result = mysqli_query($db_handle, $sql);
+            //pour les types
+
+            if($TrierType1 =='Tout Afficher')
+            {
+                $sql = "SELECT * FROM objet";
+            }
+            
+            if($TrierType2 =='Articles Rares')
+            {
+                $sql = "SELECT * FROM objet WHERE categorie ='rare'";
+            }
+            
+            if($TrierType3 =='Articles Haut de Gamme')
+            {
+                $sql = "SELECT * FROM objet WHERE categorie ='haut_de_gamme'";
+            }
+            
+            if($TrierType4 =='Articles Reguliers')
+            {
+                $sql = "SELECT * FROM objet WHERE categorie ='regulier'";
+            }
+            //pour les prix
+
+            if($TrierPrix1 =='Normal')
+            {
+                $sql = "SELECT * FROM objet";
+            }
+            
+            if($TrierPrix2 =='Prix Croissant')
+            {
+                $sql = "SELECT * FROM objet ORDER BY prix ASC";
+            }
+            
+            if($TrierPrix3 =='Prix Decroissant')
+            {
+                $sql = "SELECT * FROM objet ORDER BY prix DESC";
+            }
+            
+
+            //echo "<br>" .$sql;
+            
+
+            $result = mysqli_query($db_handle,$sql);
             
 
             echo "<div class= 'container-fluid text-center bg-grey'>";
